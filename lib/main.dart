ed0e6af0f9823bfa478fea01f17e8ca9dd52d7d4
quaku21 <quakukevin@gmail.com>
2022-09-20 03:10:40 +0000

    root of project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//root of project, test ui for room booking
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
